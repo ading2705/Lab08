049c07e5790dac3f8d2edfc276086f4f9d499cdc
Annie D <ading2@ualberta.ca>
2025-10-29 15:48:56 -0600

    passing delete city

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,6 +16,7 @@ public class CustomList {
 
     public boolean hasCity(City city){return cities.contains(city);}
 
+    public void deleteCity(City city) { cities.remove(city);}
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
