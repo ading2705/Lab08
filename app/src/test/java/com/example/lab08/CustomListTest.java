59835824d347bc45938aaf524a112cb7cfddac62
Annie D <ading2@ualberta.ca>
2025-10-29 15:48:04 -0600

    failing delete city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,13 +1,24 @@
 package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
-@Test
-    public void testHasCity(){
-    CustomList list = new CustomList();
-    City calgary = new City("Calgary", "AB");
-    list.addCity(calgary);
-    assertTrue(list.hasCity(calgary));
-}
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
 }
